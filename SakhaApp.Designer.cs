2fefdd2d813e6b61b6ce346dc7ad3fba21de5f55
Dreyfox059 <2001andrey2125@gmail.com>
2022-11-14 23:04:16 +0900

    New folder "Resources"

--- SakhaApp.Designer.cs
+++ SakhaApp.Designer.cs
@@ -37,7 +37,8 @@ namespace SakhaApp
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ProjectUnknown";
             this.Text = "SakhaApp";
